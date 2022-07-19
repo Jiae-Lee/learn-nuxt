1d905c06c893d6db136d902603c924da8ffc2fd8
Jiae-Lee <ezae94@gmail.com>
2022-07-19 22:55:42 +0900

    add backend

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>{{$route.name}} 페이지</h1>
+    <h1>{{ $route.name }} 페이지</h1>
+    <header>
+      <NuxtLink to="/">홈페이지</NuxtLink>
+      <NuxtLink to="/main">메인페이지</NuxtLink>
+      <NuxtLink to="/product">상품페이지</NuxtLink>
+    </header>
     <Nuxt />
   </div>
 </template>
